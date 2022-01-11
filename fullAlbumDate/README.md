7d726faba55b521f3b82033c969f74429e5e4b3e
huhridge <sharmaharij21@gmail.com>
2022-01-11 11:41:42 +0530

    revamped repository and added fullAlbumDate.js

---
+++ fullAlbumDate/README.md
@@ -0,0 +1,7 @@
+# Display full Album date
+Filename : `fullAlbumDate.js`
+Display full album date instead of just year
+
+![Preview](https://raw.githubusercontent.com/huhridge/huh-spicetify-extensions/main/fullAlbumDate/preview.jpg)
+
+
---
+++ fullAlbumDate/fullAlbumDate.js
@@ -0,0 +1,45 @@
+(async function fullAlbumDate() {
+    const { Player, Menu, LocalStorage, Platform } = Spicetify
+
+    async function getAlbumDate(uri) {
+        const albumInfo = await Spicetify.CosmosAsync.get(`hm://album/v1/album-app/album/${uri}/desktop`);
+        const albumDate = new Date(albumInfo.year, (albumInfo.month || 1) - 1, albumInfo.day || 0);
+        return albumDate.toLocaleString("default", { year: "numeric", month: "short", day:"numeric" });
+    }
+    
+    if (!(Player && Menu && LocalStorage && Platform)) {
+        setTimeout(fullAlbumDate, 1000)
+        return
+    }
+
+    function delay(delayInms) {
+        return new Promise(resolve => {
+          setTimeout(() => {
+            resolve(2);
+          }, delayInms);
+        });
+      }
+
+    function setDate(newDate){
+        const dateElement = document.querySelector('.main-entityHeader-divider.main-type-mesto')
+        dateElement.textContent = newDate;
+    }
+
+    if (Platform.History.location.pathname.startsWith('/album/')) {
+        const uri = Platform.History.location.pathname.split('/')[2]
+        dateElement = document.querySelector('.main-entityHeader-divider.main-type-mesto')
+        const newDate = await getAlbumDate(uri)
+        let delayinms = await delay(300)
+        setDate(newDate)
+    }
+
+    Platform.History.listen(async ({ pathname }) => {
+        if (pathname.startsWith('/album/')) {
+            const uri = pathname.split('/')[2]
+            const newDate = await getAlbumDate(uri)
+            let delayinms = await delay(300)
+            setDate(newDate)
+        }
+    })
+
+})()

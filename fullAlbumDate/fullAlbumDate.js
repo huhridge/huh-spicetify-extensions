3cd6cf04bc41ed4a13f13ba5d1f46de96d4e52d6
huhridge <sharmaharij21@gmail.com>
2022-02-20 16:39:30 +0530

    updated fullAlbumDate to add support for `1.1.79` and formatting

--- fullAlbumDate/fullAlbumDate.js
+++ fullAlbumDate/fullAlbumDate.js
@@ -1,45 +1,44 @@
 (async function fullAlbumDate() {
-    const { Player, Menu, LocalStorage, Platform } = Spicetify
+    const { Player, Menu, LocalStorage, Platform } = Spicetify;
 
     async function getAlbumDate(uri) {
         const albumInfo = await Spicetify.CosmosAsync.get(`hm://album/v1/album-app/album/${uri}/desktop`);
         const albumDate = new Date(albumInfo.year, (albumInfo.month || 1) - 1, albumInfo.day || 0);
-        return albumDate.toLocaleString("default", { year: "numeric", month: "short", day:"numeric" });
+        return albumDate.toLocaleString("default", { year: "numeric", month: "short", day: "numeric" });
     }
     
     if (!(Player && Menu && LocalStorage && Platform)) {
-        setTimeout(fullAlbumDate, 1000)
-        return
+        setTimeout(fullAlbumDate, 1000);
+        return;
     }
 
     function delay(delayInms) {
-        return new Promise(resolve => {
-          setTimeout(() => {
-            resolve(2);
-          }, delayInms);
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(2);
+            }, delayInms);
         });
-      }
+    }
 
-    function setDate(newDate){
-        const dateElement = document.querySelector('.main-entityHeader-divider.main-type-mesto')
+    function setDate(newDate) {
+        const dateElement =
+            document.querySelector(".main-entityHeader-divider.main-type-mesto") ?? document.querySelector(".main-entityHeader-metaData > span");
         dateElement.textContent = newDate;
     }
 
-    if (Platform.History.location.pathname.startsWith('/album/')) {
-        const uri = Platform.History.location.pathname.split('/')[2]
-        dateElement = document.querySelector('.main-entityHeader-divider.main-type-mesto')
-        const newDate = await getAlbumDate(uri)
-        let delayinms = await delay(1000)
-        setDate(newDate)
+    if (Platform.History.location.pathname.startsWith("/album/")) {
+        const uri = Platform.History.location.pathname.split("/")[2];
+        const newDate = await getAlbumDate(uri);
+        await delay(1000);
+        setDate(newDate);
     }
 
     Platform.History.listen(async ({ pathname }) => {
-        if (pathname.startsWith('/album/')) {
-            const uri = pathname.split('/')[2]
-            const newDate = await getAlbumDate(uri)
-            let delayinms = await delay(1000)
-            setDate(newDate)
+        if (pathname.startsWith("/album/")) {
+            const uri = pathname.split("/")[2];
+            const newDate = await getAlbumDate(uri);
+            await delay(1000);
+            setDate(newDate);
         }
-    })
-
-})()
+    });
+})();
